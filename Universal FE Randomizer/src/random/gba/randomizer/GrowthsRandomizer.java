b0803bf960531e3972dfb5e89937355fafbe5603
abbatrombone <161561688+abbatrombone@users.noreply.github.com>
2026-09-13 12:37:56 -0500

    Fix growth randomization for RES stat
    Resistance was looking at newDEFGrowth, changed to newRESGrowth
    Removed Empty case statement 0

--- Universal FE Randomizer/src/random/gba/randomizer/GrowthsRandomizer.java
+++ Universal FE Randomizer/src/random/gba/randomizer/GrowthsRandomizer.java
@@ -133,7 +133,7 @@ public class GrowthsRandomizer {
 					int amount = Math.min(5,  growthTotal);
 					int increaseAmount = 0;
 					switch (randomNum) {
-					case 0:
+					
 					case 1:
 						increaseAmount = Math.min(amount, max - newHPGrowth);
 						growthTotal -= increaseAmount;
@@ -165,7 +165,7 @@ public class GrowthsRandomizer {
 						newDEFGrowth += increaseAmount;
 						break;
 					case 7:
-						increaseAmount = Math.min(amount, max - newDEFGrowth);
+						increaseAmount = Math.min(amount, max - newRESGrowth);
 						growthTotal -= increaseAmount;
 						newRESGrowth += increaseAmount;
 						break;
